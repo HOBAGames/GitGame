da95daa310e5e0f3175c771c00d5c167d589f376
alexkuzmanov1 <36124668+alexkuzmanov1@users.noreply.github.com>
2019-05-19 16:41:03 +0300

    Update game.js

--- UROK 11/game.js
+++ UROK 11/game.js
@@ -1,20 +1,42 @@
-// Creating variables
-var myX = 0, myY = 0,updates=0;
-function update() {
-    // Napisanoto tuk se izpulnqva otnovo i otnovo mnogo puti v sekunda
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+class PlayerIvan(){
+    constructor(){
+        this.y=150;
+        this.x=150;
+        this.speed=10;
+    }
+    
+    
+    update(){
+        if(isKeyPressed[87]){
+            this.y-=this.speed;
+        }
+        
+        if(isKeyPressed[83]){
+            this.y+=this.speed;
+        }
+        
+        if(isKeyPressed[65]){
+            this.x-=this.speed;
+        }
+        
+        if(isKeyPressed[68]){
+            this.x+=this.speed;
+        }
+    }
+    
+    draw(){
+        context.fillRect(this.x,this.y,110,110);
+    }
+  
 }
-function draw() {
-    // tuk naprogramirai kakvo da se risuva
-    drawImage(backCake,0, 0, 800, 600);
-    drawImage(femaleAction,myX,myY,80,110);
-};
-function keyup(key) {
-    // Show the pressed keycode in the console
-    console.log("Pressed", key);
-};
-function mouseup() {
-    // Show coordinates of mouse on click
-    console.log("Mouse clicked at", mouseX, mouseY);
-};
+
+
+b = new PlayerIvan();
+
+function update(){
+    a.update();
+}
+
+function draw(){
+    a.draw();
+}
